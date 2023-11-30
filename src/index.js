6629d9fb00fc4d86d0d50d0f89888baa2b40affe
DaniDeveloper11 <angel.montes0498@gmail.com>
2023-11-29 22:12:42 -0600

    Creacion de Observador y Observable

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import { Observable } from 'rxjs'
+
+const observableAlfa$ = new Observable(subscriber =>{
+    subscriber.next(1);
+  
+    subscriber.next(2);
+    subscriber.complete();
+    subscriber.next(20);
+
+});
+
+const observador = {
+    next: (value) => {
+        console.log(value);
+    },
+    complete: () => {
+        console.log("ya termine, estuvo rico")
+    },
+    error: (error) => {
+        console.log('error emitido')
+        console.error(error);
+    }
+};
+
+observableAlfa$.subscribe(observador);
